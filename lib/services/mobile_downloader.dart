8c09abe9a14b30cd12f5994187dc95781e4349b8
muhamed-ramic <muhamed.ramic@edu.fit.ba>
2023-07-25 20:31:09 +0200

    add mobile downloading service with error handling

--- lib/services/mobile_downloader.dart
+++ lib/services/mobile_downloader.dart
@@ -1,7 +1,6 @@
-import 'dart:typed_data';
 import 'dart:io';
 
-import 'package:file_saver/file_saver.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -11,22 +10,41 @@ class MobileDownloadService implements DownloadService {
   @override
   Future<void> download({required List<int> bytes, String? content}) async {
     // requests permission for downloading the file
-    bool hasPermission = await _requestWritePermission();
-    if (!hasPermission) return;
-    // gets the directory where we will download the file.
-    var dir = await getApplicationDocumentsDirectory();
+    try {
+      bool hasPermission = await _requestWritePermission();
+      if (!hasPermission) return;
+      // gets the directory where we will download the file.
+      var dir = await getApplicationDocumentsDirectory();
 
-    if (Platform.isAndroid) {
-      dir = Directory("/storage/emulated/0/Download");
+      if (Platform.isAndroid) {
+        dir = Directory("/storage/emulated/0/Download");
+      }
+      var savedFolder = "Download";
+      if (Platform.isIOS) {
+        savedFolder = "Documents";
+      }
+
+      // You should put the name you want for the file here.
+      // Take in account the extension.
+      String fileName = 'config-customly.txt';
+
+      // saving the file
+      File file = File('${dir.path}/$fileName');
+      await file.writeAsString(content as String);
+      Fluttertoast.showToast(
+          msg: "Successfully saved the file in: " + savedFolder + " folder",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 3,
+          fontSize: 16.0);
+    } catch (error) {
+      Fluttertoast.showToast(
+          msg: "Error occured while downloading file",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 3,
+          fontSize: 16.0);
     }
-
-    // You should put the name you want for the file here.
-    // Take in account the extension.
-    String fileName = 'config-customly';
-
-    // saving the file
-    File file = File('${dir.path}/$fileName');
-    await file.writeAsString(content as String);
   }
 
   Future<bool> _requestWritePermission() async {
